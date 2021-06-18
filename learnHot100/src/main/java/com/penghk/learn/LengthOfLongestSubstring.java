9979b299e100672d04e017b2333b3ffa8207e4a9
penghangkong <xmly@penghangkongdeMacBook-Pro.local>
2021-06-18 18:02:45 +0800

    f

---
+++ learnHot100/src/main/java/com/penghk/learn/LengthOfLongestSubstring.java
@@ -0,0 +1,50 @@
+package com.penghk.learn;
+
+import java.util.BitSet;
+
+/**
+ * @Author:hangkong.peng
+ * @Date: 2021/6/18 16:44
+ */
+public class LengthOfLongestSubstring {
+
+
+    public int lengthOfLongestSubstring(String s) {
+
+        int longestlen = 0;
+        BitSet charSet = new BitSet();
+
+        int subLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (longestlen >= s.length() - i) {
+                return longestlen;
+            }
+
+            for (int j = i; j < s.length(); j++) {
+
+                if (charSet.get(s.charAt(j))) {
+                    //already have this char
+                    if (subLen >  longestlen) {
+                        longestlen = subLen;
+                    }
+                    charSet.clear();
+                    subLen = 0;
+                    break;
+                } else {
+                    charSet.set(s.charAt(j));
+                    subLen += 1;
+                    if ((j == s.length() - 1) && subLen > longestlen) {
+                        longestlen = subLen;
+                    }
+                }
+
+            }
+        }
+
+        return longestlen;
+    }
+
+
+
+}
